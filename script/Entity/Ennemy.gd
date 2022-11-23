664565f3284bee7da62e8daf25c017651f5e43e9
Alexandre L-P <aloupoueyou@DESKTOP-4R78PDH.localdomain>
2022-11-23 23:39:22 +0100

    [Add]: MainMenu

--- script/Entity/Ennemy.gd
+++ script/Entity/Ennemy.gd
@@ -18,10 +18,8 @@ func _ready():
 	var tree = get_tree()
 	if tree.has_group("LevelNavigation"):
 		levelNavigation = tree.get_nodes_in_group("LevelNavigation")[0]
-		#levelNavigation = tree.get_nodes_in_groups("LevelNavigation")[0]
 	if tree.has_group("Character"):
 		character = tree.get_nodes_in_group("Character")[0]
-		#character = tree.get_nodes_in_group("Character")[0]
 
 func _physics_process(delta):
 	Line.global_position = Vector2.ZERO
@@ -39,27 +37,9 @@ func navigate():
 		if global_position == path[0]:
 			path.pop_front()
 
-func position_avaible_around_character() -> Vector2:
-	var x;
-	var y;
-	x = rand_range((character.global_position.x - 200), (character.global_position.x + 200))
-	y = rand_range((character.global_position.y - 200), (character.global_position.y + 200))
-	return Vector2(x, y)
-
 func generate_path():
 	if levelNavigation != null and character != null:
-		if not chasing:
-			print("Hey 1")
-			var arr = position_avaible_around_character()
-			if not $Timer.is_stopped():
-				print(" WTF The timer is currently playing")
-			else :
-				print("Hello")
-				path = levelNavigation.get_simple_path(global_position, arr, false)
-				$Timer.start()
-		elif chasing : 
-			path = levelNavigation.get_simple_path(global_position, character.global_position, false)
-		
+		path = levelNavigation.get_simple_path(global_position, character.global_position, false)
 
 func move():
 	velocity = move_and_slide(velocity)
